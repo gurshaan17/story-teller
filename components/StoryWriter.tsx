f1d073c6cb3c5620504017cf100b2125fc9c10e1
gurshaan17 <gurshaansinghd@gmail.com>
2024-10-28 14:11:30 +0530

    landing page look completed

--- components/StoryWriter.tsx
+++ components/StoryWriter.tsx
@@ -1,15 +1,49 @@
 'use client'
 
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
 import { Textarea } from "./ui/textarea"
+import { pages } from "next/dist/build/templates/app-page";
 
 function StoryWriter() {
+
+  const [story, setStory] = useState<string>("")
+  const [pages, setPages] = useState<number>()
+
   return (
-    <div className="flex flex-col container">
+    <div className="flex flex-col container p-5">
         <section className="flex-1 flex flex-col border border-purple-300 rounded-md  p-10 space-y-2">
-            <Textarea />
+            <Textarea
+              value={story}
+              onChange={(e) => {setStory(e.target.value)}}
+             className="flex-1 text-black" placeholder='Write a story about a dog and a robot that become friends .....'
+            />
+            <Select onValueChange={(e) => {setPages(parseInt(e))}}>
+              <SelectTrigger>
+                <SelectValue placeholder={"How many pages should the story be?"}/>
+              </SelectTrigger>
+              <SelectContent className="w-full">
+                {Array.from({ length: 10}, (_, i) => (
+                  <SelectItem key={i} value={String(i+1)}>
+                    {i+1}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            <Button disabled={!story || !pages} className="w-full " size="lg">
+              Generate Story
+            </Button>
+        </section>
+        <section className="flex-1 pb-5 mt-5">
+          <div className="flex flex-col-reverse w-full space-y-2 bg-gray-800 rounded-md text-gray-200 font-mono p-10 h-96 overflow-y-scroll">
+            <div>
+              <span className="mr-5">{">>"}</span>
+            </div>
+          </div>
         </section>
     </div>
   )
 }
 
-export default StoryWriter
+export default StoryWriter;
