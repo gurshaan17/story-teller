5e6e4a9b74f65c6b7b0bc57d8fc3fa4aa9ad0df9
gurshaan17 <gurshaansinghd@gmail.com>
2024-10-31 23:47:14 +0530

    key added

--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -14,7 +14,11 @@ function Stories(){
         {stories.length == 0 && <p>No stories found</p>}
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
             {stories.map(story => (
-                <Link href={`/stories/${story.story}`} className="border rounded-lg cursor-pointer hover:shadow-lg hover:border-purple-500 transition-all duration-300 ease-in-out">
+                <Link 
+                    key={story.story}
+                    href={`/stories/${story.story}`} 
+                    className="border rounded-lg cursor-pointer hover:shadow-lg hover:border-purple-500 transition-all duration-300 ease-in-out"
+                >
                     <div className="relative ">
                         <p className="absolute flex items-center top-0 right-0 bg-white text-purple-500 font-bold p-3 rounded-lg m-2 text-sm">
                             <BookOpen className="w-4 h-4 mr-1" />
