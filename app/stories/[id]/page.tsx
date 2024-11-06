02fa3404888c23c4c6cf5268796e539565745bb7
gurshaan17 <gurshaansinghd@gmail.com>
2024-11-06 20:06:45 +0530

    type error

--- app/stories/[id]/page.tsx
+++ app/stories/[id]/page.tsx
@@ -4,36 +4,25 @@ import { notFound } from "next/navigation";
 
 interface StoryPageProps {
   params: {
-    id: string;
-  };
-}
-
-// Fetches the decoded story ID
-async function getParams(params: StoryPageProps["params"]): Promise<string> {
-  const decodedId = decodeURIComponent(params.id);
-  return decodedId;
-}
-
-async function StoryPage({ params }: StoryPageProps) {
-  const storyId = await getParams(params);
-  const story = await getStory(storyId);
-  
-  if (!story) {
-    return notFound();
+    id: string
   }
+}
 
-  return (
-    <div>
-      <Story story={story} />
-    </div>
-  );
+function StoryPage({params: {id}}: StoryPageProps){
+  const decodedId = decodeURIComponent(id);
+  const story = getStory(decodedId);
+  if(!story){
+    return notFound()
+  }
+  return <Story story={story} />
 }
 
 export default StoryPage;
 
 export async function generateStaticParams() {
   const stories = getAllStories();
-  return stories.map((story) => ({
-    id: story.story, // Use the correct identifier for each story
-  }));
+  const paths = stories.map((story) => ({
+    id: story.story
+  }))
+  return paths;
 }
